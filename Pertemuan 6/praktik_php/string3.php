b81b1a3a4794425d1e978e2be33d0bdddb8db85a
bagas farel andrean <bagasfarel45@gmail.com>
2024-03-26 08:32:31 +0700

    upload pertemuan 6

---
+++ Pertemuan 6/praktik_php/string3.php
@@ -0,0 +1,16 @@
+<?php
+
+/*$pesan = "Saya arek mojosari";
+echo strrev($pesan) . "<br>"; */
+
+$pesan = "saya arek mojosari";
+# ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+# gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
+
+?>
