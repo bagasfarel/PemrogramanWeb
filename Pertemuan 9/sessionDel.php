01749f83d1083abc72fb110761c1acac09994976
bagas farel andrean <bagasfarel45@gmail.com>
2024-05-04 20:44:12 +0700

    upload pertemuan 9

---
+++ Pertemuan 9/sessionDel.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <?php
+    session_unset();
+    session_destroy();
+
+    echo "All session variable are now removed, and the session is destroyed.";
+    ?>
+</body>
+
+</html>
